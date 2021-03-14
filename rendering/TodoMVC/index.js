7b8aa61dd5a7eef0f328a28e8a245e41638120fe
sim-eunji <kingeunji12@gmail.com>
2021-03-15 00:55:45 +0900

    Feature; Add index.js view-controller 작성 및 getTodos.js 추가

--- rendering/TodoMVC/index.js
+++ rendering/TodoMVC/index.js
@@ -1,50 +1,14 @@
-// 필터링된 todo 리스트를 가진 ui
-const getTodoElement = (todo) => {
-  const { text, completed } = todo
-  return `
-    <li ${completed ? 'class="completed"' : ''}>
-      <div class="view">
-        <input
-          ${completed ? 'checked' : ''}
-          class="toggle"
-          type="checkbox">
-        <label> ${text} </label>
-        <button class="destory"> </button>
-      </div>
-      <input class="edit" value="${text}">
-    </li>
-  `
+import getTodos from './getTodos.js'
+import view from './view.js'
+
+const state = {
+  todos: getTodos(),
+  currentFilter: 'All'
 }
 
-// 완료되지 않은 todo 수의 span
-const getTodoCount = (todos) => {
-  const notCompleted = todos.filter(todo => !todo.completed)
-  const { length } = notCompleted
+const main = document.querySelector('.todoapp')
 
-  if (length === 1) return '1 Items left'
-
-  return `${length} Items left`
-}
-
-export default (targetElement, state) => {
-  const { currentFilter, todos } = state
-
-  const element = targetElement.cloneNode(true)
-  const list = element.querySelector('.todo-list')
-  const counter = element.querySelector('.todo-count')
-  const filters = element.querySelector('.filters')
-
-  list.innerHTML = todos.map(getTodoElement).join('')
-  counter.textContent = getTodoCount(todos)
-
-  Array.from(filters.querySelectorAll('li a'))
-    .forEach(a => {
-      if (a.textContent === currentFilter) {
-        a.classList.add('selected')
-      } else {
-        a.classList.remove('selected')
-      }
-    })
-
-  return element
-}
+window.requestAnimationFrame(() => {
+  const newMain = view(main, state)
+  main.replaceWith(newMain)
+})
